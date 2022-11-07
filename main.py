f963c664cfa53060f841bc29a54cbac34bb3b4cc
MythicalPlayz <57963367+MythicalPlayz@users.noreply.github.com>
2022-11-07 13:54:44 +0200

    code

---
+++ main.py
@@ -0,0 +1,48 @@
+#Copyright 2022
+#Author: MythticalPlayz
+#Code: Log in System and check if the user is admin
+
+#import the json file with the admins
+import json
+f = open("admins.json")
+admins = json.load(f)
+
+#set the login to false
+loggedin = False
+
+#import username from user
+name = input("Enter your name \n")
+
+#loop through the admins
+for i in range(0,dict.__len__(admins)):
+    admintocheck = "admin" + str(i)
+
+    #check if admin[number] exists
+    if dict.get(admins,admintocheck):
+
+         #get its username
+       adminname = dict.get(dict.get(admins,admintocheck),"name")
+       if adminname == name:
+
+        #get the password for the account
+        pswrd = input("To verify that you are an Admin, Please enter your password:\n")
+        if pswrd == dict.get(dict.get(admins,admintocheck),"password"):
+
+            #log in the user as Admin
+            print("Welcome: ",name,"\nRank: Admin")
+            loggedin = True
+            break
+        else:
+
+             #password incorrect; log in the user as a user
+            print("Error: Wrong Password \nLogging in as a user")
+            break    
+
+#See if user logged in 
+if not loggedin:
+
+    #log in the user as a user
+    print("Welcome: ",name,"\nRank: User")
+
+#flush memory
+f.flush()
